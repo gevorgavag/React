5b81736182f36485138cb979d6f90b59eec309d2
gevorgavag <gevorgavagyan34@gmail.com>
2024-05-05 18:48:35 +0400

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://marketplace.canva.com/EAFauoQSZtY/1/0/1600w/canva-brown-mascot-lion-free-logo-qJptouniZ0A.jpg"/>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://static.vecteezy.com/ti/photos-gratuite/p1/13485800-fond-de-voyage-d-ete-ile-de-plage-tropicale-exotique-cote-paradisiaque-palmiers-sable-blanc-ciel-incroyable-ocean-lagon-fantastique-fond-de-nature-magnifique-journee-ensoleillee-vacances-idylliques-inspirantes-photo.jpg"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                my post
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
