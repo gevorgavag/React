30dae6868408913ed19e138ac2e36d7d3db80e24
gevorgavag <gevorgavagyan34@gmail.com>
2024-05-05 19:13:21 +0400

    Add className in Header components

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
     return (
         <header className="header">
             <img
+                className="headerImg"
                 src="https://marketplace.canva.com/EAFauoQSZtY/1/0/1600w/canva-brown-mascot-lion-free-logo-qJptouniZ0A.jpg"/>
         </header>
     );
